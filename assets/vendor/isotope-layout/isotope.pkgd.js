96c705b1d32da45208aa136788edb615d2f7c28f
Getinet Amare <getinetamare@gmail.com>
2022-11-11 03:06:30 +0300

    Remove duplicate degree and unecessery file 1

--- assets/vendor/isotope-layout/isotope.pkgd.js
+++ assets/vendor/isotope-layout/isotope.pkgd.js
@@ -353,7 +353,7 @@ function getStyle( elem ) {
   if ( !style ) {
     logError( 'Style returned ' + style +
       '. Are you running this code in a hidden iframe on Firefox? ' +
-      'See https://bit.ly/getsizebug1' );
+      '' );
   }
   return style;
 }
